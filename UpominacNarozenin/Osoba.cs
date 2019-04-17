17750f2a051a557e65ada66c6eca952720e1ddca
Tomas <tomas.stranak@accenture.com>
2019-04-17 15:21:10 +0200

    added backend logic classes Osoba and SpravceOsob

---
+++ UpominacNarozenin/Osoba.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace UpominacNarozenin
+{
+    public class Osoba
+    {
+        public string Jmeno { get; set; }
+        public DateTime Narozeniny { get; set; }
+        public int Vek
+        {
+            get
+            {
+                DateTime dnes = DateTime.Today;
+                Console.WriteLine(dnes);
+                int vek = dnes.Year - Narozeniny.Year;
+                Console.WriteLine(vek);
+                if (dnes < Narozeniny.AddYears(vek))
+                    vek--;
+                Console.WriteLine(vek);
+                return vek;
+            }
+        }
+
+        public int ZbyvaDni
+        {
+            get
+            {
+                DateTime dnes = DateTime.Today;
+                Console.WriteLine(dnes);
+                DateTime dalsiNarozeniny = Narozeniny.AddYears(Vek + 1);
+                Console.WriteLine(dalsiNarozeniny);
+                TimeSpan rozdil = dalsiNarozeniny - DateTime.Today;
+                Console.WriteLine(rozdil);
+
+                return Convert.ToInt32(rozdil.TotalDays);
+            }
+        }
+
+        /// <param name="jmeno">Jméno</param>
+        /// <param name="narozeniny">Datum narození</param>
+        public Osoba(string jmeno, DateTime narozeniny)
+        {
+            this.Jmeno = jmeno;
+            this.Narozeniny = narozeniny;
+        }
+
+        /// <returns>Textová reprezentace osoby</returns>
+        public override string ToString()
+        {
+            return Jmeno;
+        }
+    }
+}
---
+++ UpominacNarozenin/SpravceOsob.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace UpominacNarozenin
+{
+    public class SpravceOsob
+    {
+        public ObservableCollection<Osoba> Osoby { get; set; }
+
+        public SpravceOsob()
+        {
+            Osoby = new ObservableCollection<Osoba>();
+        }
+
+        public Osoba NejblizsiOsoba { get; set; }
+
+        public DateTime DnesniDatum
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+        private void NajdiNejblizsi()
+        {
+            var serazeneOsoby = Osoby.OrderBy(o => o.ZbyvaDni);
+            if (serazeneOsoby.Count() > 0)
+                NejblizsiOsoba = serazeneOsoby.First();
+            else
+                NejblizsiOsoba = null;
+        }
+        public void Pridej(string jmeno, DateTime? datumNarozeni)
+        {
+            if (jmeno.Length < 3)
+                throw new ArgumentException("Jméno je příliš krátké");
+            if (datumNarozeni == null)
+                throw new ArgumentException("Nebylo zadané datum narození");
+            if (datumNarozeni.Value.Date > DateTime.Today)
+                throw new ArgumentException("Datum narození nesmí být v budoucnosti");
+            Osoba osoba = new Osoba(jmeno, datumNarozeni.Value.Date);
+            Osoby.Add(osoba);
+            NajdiNejblizsi();
+        }
+        public void Odeber(Osoba osoba)
+        {
+            Osoby.Remove(osoba);
+            NajdiNejblizsi();
+        }
+    }
+}
